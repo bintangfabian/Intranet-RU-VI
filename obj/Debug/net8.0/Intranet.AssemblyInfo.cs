7468ed468a0d7b28ab8cd8627c2981ba76df166e
gilangriyanto <gilangriyanto27@gmail.com>
2024-08-01 16:04:04 +0700

    update sidemenu

--- obj/Debug/net8.0/Intranet.AssemblyInfo.cs
+++ obj/Debug/net8.0/Intranet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intranet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53eca053c8d8a2aea48e74544467c22d04e2a7d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d192d434ab99e5b1dd94d34faadb58302f4142d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intranet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intranet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
