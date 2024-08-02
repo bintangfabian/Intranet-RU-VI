048af40a4848abf38e61e0e6cfaf913d96df2415
gilangriyanto <gilangriyanto27@gmail.com>
2024-08-02 16:02:03 +0700

    update home

--- obj/Debug/net8.0/Intranet.AssemblyInfo.cs
+++ obj/Debug/net8.0/Intranet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intranet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d192d434ab99e5b1dd94d34faadb58302f4142d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7468ed468a0d7b28ab8cd8627c2981ba76df166e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intranet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intranet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
