eb1a042804646d70a9bef061bc17ce51a7d998e4
gilangriyanto <gilangriyanto27@gmail.com>
2024-08-12 16:02:58 +0700

    menambah konten sidebar

--- obj/Debug/net8.0/Intranet.AssemblyInfo.cs
+++ obj/Debug/net8.0/Intranet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intranet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c0bd0473ab86b2d2fae25beee180f14a47880ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81b16d67ad397f03cde3d762c39edbe83a47da0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intranet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intranet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
